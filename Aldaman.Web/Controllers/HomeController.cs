6bf99cae2979ae83b7bd72b6a11ae063e4da925d
Martin Zaloga <martin.zaloga@gmail.com>
2026-03-31 11:17:48 +0200

    Add project files.

---
+++ Aldaman.Web/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Aldaman.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aldaman.Web.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
