5763744a6dab6d050228968118b0b17656c589db
Martin Zaloga <martin.zaloga@gmail.com>
2026-06-19 13:02:35 +0200

    FormatConstants and ussage in views.

---
+++ Aldaman.Services/Constants/FormatConstants.cs
@@ -0,0 +1,13 @@
+namespace Aldaman.Services.Constants;
+
+public static class FormatConstants
+{
+    public const string DateIso = "yyyy-MM-dd";
+    public const string DateDot = "d. M. yyyy";
+    public const string TimeShort = "HH:mm";
+    public const string DateTimeDot = "d. M. yyyy HH:mm";
+    public const string DateTimeIso = "yyyy-MM-dd HH:mm:ss";
+    public const string FileSizeDecimal = "N1";
+    public const string ShortDateTime = "g";
+    public const string FullDateTime = "f";
+}
